3c5b3358f17c496d8f1f35d5700ad1a2e799dc68
lsy <tmddus7010@gmail.com>
2022-05-03 09:57:02 +0900

    게임시작, 점수기능 추가

--- snake.js
+++ snake.js
@@ -7,6 +7,7 @@ const snakeLoad = function () {
         snake_table : document.querySelector('[data-snake-table]'),
         snake_view : document.querySelector('[data-snake-snake]'),
         apple : document.querySelector('[data-apple]'),
+        score : document.querySelector('[data-now]'),
         snake_size : window.getComputedStyle(document.querySelector('[data-snake-snake]')).width.replace('px', ''),
         move : 1,
         apple_loaction : 0,
@@ -23,9 +24,9 @@ const snakeLoad = function () {
             
             //사과 랜덤
             self.randomApple();
-            
-            //스네이크 동작
-            self.moveSnake();
+
+            //스네이크 게임 시작
+            self.snakeGameStart();
         },
         //스네이크 세팅
         //스네이크 맵 꾸미기(div에 width값을 줘서 흰색 테두리를 준다)
@@ -35,6 +36,10 @@ const snakeLoad = function () {
             document.querySelector('.box-top').style.width = snake.snake_size * x + 'px';
             document.querySelector('.box-bottom').style.width = snake.snake_size * x + 'px';
             document.querySelector('.box-bottom').style.height = snake.snake_size * y + 'px';
+
+            //기존 점수가 있다면 점수 불러옴
+            const best_score = localStorage.getItem('score');
+            if(best_score) document.querySelector('[data-best]').innerHTML = best_score;
 
             //스네이크 보여주기
             self.reloadSnake();
@@ -46,8 +51,13 @@ const snakeLoad = function () {
             const center_x = Math.ceil(x/ 4) - 1 ;
             const center_y = Math.ceil(y/ 2) - 1 ;
             snake.snake_arr = [ center_y * y + center_x ];
-            //화면에 노출
+           
+            //초기화
+            snake.move = 1;
+            snake.score.innerHTML = 0;
             document.querySelectorAll('[data-snake-table] [data-snake-snake]').forEach((elem) => elem.remove());;
+
+            //화면에 노출
             self.loadSnake(snake.snake_arr[0]);
         },
         //스네이크 보여주기(추가되는 경우)
@@ -74,45 +84,19 @@ const snakeLoad = function () {
             snake.apple_loaction = location;
 
         },
+        //스네이크 게임 시작
+        snakeGameStart : function(){
+            const button = document.querySelector('[data-start]');
+
+            button.addEventListener('click', event => {
+                //스네이크 동작
+                self.moveSnake();
+            });
+        },
         //스네이크 움직이기
         moveSnake : function(){
 
-            /* setInterval(() => {
-
-                const snakeArr = document.querySelectorAll('[data-snake-table] [data-snake-snake]');
-                const firstSnake = snakeArr[0];//뱀의 머리
-                const lastSnake = snakeArr[snakeArr.length - 1];//뱀의 꼬리
-
-                //실제 값 변경
-                snake.snake_arr.unshift(snake.snake_arr[0] + snake.move);
-                snake.snake_arr.length = snake.snake_arr.length - 1;
-
-                //사과 먹은 경우
-                console.log("apple" + snake.apple_loaction);
-                console.log(snake.snake_arr);
-                console.log(snake.snake_arr.includes(snake.apple_loaction));
-                //먹지 않은 경우
-
-                //실제로 이동
-                if(snake.move == 1){ //오른쪽
-                    firstSnake.style.left = (  Number(lastSnake.style.left.replace('px', '')) + Number(snake.snake_size) ) + 'px';
-                }else if(snake.move == -1){ //왼쪽
-                    firstSnake.style.left = (  Number(lastSnake.style.left.replace('px', '')) - Number(snake.snake_size) ) + 'px';
-                }else if(snake.move == -x){ //위
-                    firstSnake.style.top = (  Number(lastSnake.style.top.replace('px', '')) - Number(snake.snake_size) ) + 'px';
-                }else if(snake.move == x){ //아래
-                    firstSnake.style.top = (  Number(lastSnake.style.top.replace('px', '')) + Number(snake.snake_size) ) + 'px';
-                }
-                snake.snake_table.insertBefore(lastSnake, firstSnake); 
-
-            }, 1000);*/
-
-            //키보드값에 따라 제어, 반대편 입력하지 않은 경우에만 가능
-            window.onkeydown = (e) => {
-                if(e.code == 'ArrowUp' && snake.move != y){ snake.move = -y; }
-                if(e.code == 'ArrowDown' && snake.move != -y){ snake.move = y; }
-                if(e.code == 'ArrowLeft' && snake.move != 1){ snake.move = -1; }
-                if(e.code == 'ArrowRight' && snake.move != -1){ snake.move = 1; }
+            var interval = setInterval(() => {
 
                 const snake_view_arr = document.querySelectorAll('[data-snake-table] [data-snake-snake]');
                 const first_snake = snake_view_arr[0];//뱀의 머리
@@ -125,7 +109,9 @@ const snakeLoad = function () {
                 if( ( snake_x == (x - 1) && snake.move == 1 ) || ( snake_x == 0 && snake.move == -1 )
                     || ( snake_y == (y - 1) && snake.move == y ) || ( snake_y == 0 && snake.move == -y) 
                     || snake.snake_arr.includes(move_snake) ){
-                    self.gameOver(); return false;
+                    clearInterval(interval);
+                    self.gameOver();
+                    return false;
                 }
 
                 //실제 값 변경
@@ -133,6 +119,9 @@ const snakeLoad = function () {
 
                 //사과 먹은 경우
                 if(snake.snake_arr[0] == snake.apple_loaction){
+                    //점수 카운팅
+                    snake.score.innerHTML = Number(snake.score.innerText) + 1 ;
+
                     self.loadSnake(snake.apple_loaction);
                     self.randomApple();
                 }else{//먹지 않은 경우
@@ -154,12 +143,25 @@ const snakeLoad = function () {
                     snake.snake_arr.length = snake.snake_arr.length - 1;
                 }
 
+            }, 500);
+
+            //키보드값에 따라 제어, 반대편 입력하지 않은 경우에만 가능
+            window.onkeydown = (e) => {
+                if(e.code == 'ArrowUp' && snake.move != y){ snake.move = -y; }
+                if(e.code == 'ArrowDown' && snake.move != -y){ snake.move = y; }
+                if(e.code == 'ArrowLeft' && snake.move != 1){ snake.move = -1; }
+                if(e.code == 'ArrowRight' && snake.move != -1){ snake.move = 1; }
             };
 
-
         },
-        //게임오버 함수, 게임 초기화
+        //게임오버 함수, 게임 초기화, 최고 점수 설정
         gameOver : function(){
+            const best_score = localStorage.getItem('score');
+            const now_score = Number(snake.score.innerText);
+            if( !best_score || best_score < now_score ){
+                localStorage.setItem('score', now_score);
+                document.querySelector('[data-best]').innerHTML = now_score;
+            }
             alert('gameOver');
             self.reloadSnake();
         }
